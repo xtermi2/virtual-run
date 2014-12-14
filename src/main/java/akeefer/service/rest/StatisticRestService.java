1a98ff34095d89c7c009c60b439f337ee5da1c64
xtermi2 <keefer.a@gmx.de>
2014-12-14 22:22:40 +0100

    - cron tasks absichern

--- src/main/java/akeefer/service/rest/StatisticRestService.java
+++ src/main/java/akeefer/service/rest/StatisticRestService.java
@@ -2,7 +2,10 @@ package akeefer.service.rest;
 
 import akeefer.model.BenachrichtigunsIntervall;
 import akeefer.service.PersonService;
+import akeefer.web.WicketApplication;
+import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.injection.Injector;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,8 @@ import org.wicketstuff.rest.annotations.MethodMapping;
 import org.wicketstuff.rest.annotations.ResourcePath;
 import org.wicketstuff.rest.resource.gson.GsonRestResource;
 import org.wicketstuff.rest.utils.http.HttpMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ResourcePath("/rest")
 public class StatisticRestService extends GsonRestResource {
@@ -26,6 +31,23 @@ public class StatisticRestService extends GsonRestResource {
     @MethodMapping(value = "/statistic/{intervall}", httpMethod = HttpMethod.GET)
     public void get(String intervall) {
         try {
+            boolean isLocalMode = RuntimeConfigurationType.DEVELOPMENT.equals(WicketApplication.get().getConfigurationType());
+            HttpServletRequest request = ((HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest());
+
+            if (isLocalMode) {
+                logger.info("local request (RuntimeConfigurationType.DEVELOPMENT)");
+            } else {
+                logger.info("prod request (RuntimeConfigurationType.DEPLOYMENT)");
+            }
+
+            String appEngineCronFlag = request.getHeader("X-AppEngine-Cron");
+            if ("true".equalsIgnoreCase(appEngineCronFlag)) {
+                logger.info("X-AppEngine-Cron=true, now we are sure this request is from GAE Cron");
+            } else {
+                logger.info("X-AppEngine-Cron=" + appEngineCronFlag);
+            }
+            // TODO (ak) Service nur lokal und wenn X-AppEngine-Cron=true ist ausfuehren
+
             personService.sendStatisticMail(BenachrichtigunsIntervall.valueOf(intervall));
         } catch (Exception e) {
             logger.warn("Fehler bei sendStatisticMail: " + e.getMessage(), e);
