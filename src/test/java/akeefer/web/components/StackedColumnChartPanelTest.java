121f403f467a97325c5f5fef74e028f4b6a85f28
Andreas Keefer <xtermi2@users.noreply.github.com>
2020-03-15 12:11:12 +0100

    - fixed locale specific unittest - updated assertj version - added build badge to README.md

--- src/test/java/akeefer/web/components/StackedColumnChartPanelTest.java
+++ src/test/java/akeefer/web/components/StackedColumnChartPanelTest.java
@@ -5,41 +5,63 @@ import org.joda.time.DateTime;
 import org.joda.time.Interval;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 public class StackedColumnChartPanelTest {
 
     @Test
     public void testGetIntervall() throws Exception {
         Interval intervall = ChartIntervall.Woche.getIntervall();
-        assertEquals(false, intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1)));
-        assertEquals(true, intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusMillis(1)));
-        assertEquals(true, intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusWeeks(1)));
-        assertEquals(true, intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusDays(7)));
-        assertEquals(false, intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusWeeks(1).minusMillis(1)));
+        assertThat(intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1)))
+                .isEqualTo(false);
+        assertThat(intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusMillis(1)))
+                .isEqualTo(true);
+        assertThat(intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusWeeks(1)))
+                .isEqualTo(true);
+        assertThat(intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusDays(7)))
+                .isEqualTo(true);
+        assertThat(intervall.contains(new DateTime().withMillisOfDay(0).plusDays(1).minusWeeks(1)
+                .minusMillis(1))).isEqualTo(false);
     }
 
     @Test
     public void testGetIntervallGesamt() throws Exception {
         Interval intervall = ChartIntervall.Gesamt.getIntervall();
         DateTime base = new DateTime().withMillisOfDay(0).withDayOfYear(1).plusYears(1);
-        assertEquals(false, intervall.contains(base));
-        assertEquals(true, intervall.contains(base.minusMillis(1)));
+        assertThat(intervall.contains(base))
+                .isEqualTo(false);
+        assertThat(intervall.contains(base.minusMillis(1)))
+                .isEqualTo(true);
     }
 
     @Test
     public void testGetIntervallJahr() throws Exception {
         Interval intervall = ChartIntervall.Jahr.getIntervall();
         DateTime base = new DateTime().withMillisOfDay(0).withDayOfMonth(1).plusMonths(1);
-        assertEquals(false, intervall.contains(base));
-        assertEquals(true, intervall.contains(base.minusMillis(1)));
+        assertThat(intervall.contains(base))
+                .isEqualTo(false);
+        assertThat(intervall.contains(base.minusMillis(1)))
+                .isEqualTo(true);
     }
 
     @Test
     public void testGetBeschreibung() throws Exception {
-        assertEquals("Do 1.", ChartIntervall.Woche.getBeschreibung(new Interval(new DateTime(2015, 1, 1, 0, 0, 0, 0), new DateTime())));
-        assertEquals("1. Jan", ChartIntervall.Monat.getBeschreibung(new Interval(new DateTime(2015, 1, 1, 0, 0, 0, 0), new DateTime())));
-        assertEquals("Jan 2015", ChartIntervall.Jahr.getBeschreibung(new Interval(new DateTime(2015, 1, 1, 0, 0, 0, 0), new DateTime())));
-        assertEquals("2015", ChartIntervall.Gesamt.getBeschreibung(new Interval(new DateTime(2015, 1, 1, 0, 0, 0, 0), new DateTime())));
+        Interval interval = new Interval(new DateTime(2015, 1, 1, 0, 0, 0, 0), new DateTime());
+
+        assertSoftly(softly -> {
+            softly.assertThat(ChartIntervall.Woche.getBeschreibung(interval))
+                    .as("week")
+                    .isIn("Do 1.", "Thu 1.");
+            softly.assertThat(ChartIntervall.Monat.getBeschreibung(interval))
+                    .as("month")
+                    .isEqualTo("1. Jan");
+            softly.assertThat(ChartIntervall.Jahr.getBeschreibung(interval))
+                    .as("year")
+                    .isEqualTo("Jan 2015");
+            softly.assertThat(ChartIntervall.Gesamt.getBeschreibung(interval))
+                    .as("total")
+                    .isEqualTo("2015");
+        });
     }
 }
